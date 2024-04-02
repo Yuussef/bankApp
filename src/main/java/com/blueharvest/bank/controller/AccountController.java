503296696650a8b3d37c319b6f024d1fdc0bcd08
Yuussef <chahbiyuussef@gmail.com>
2024-04-02 23:43:46 +0200

    add swagger

--- src/main/java/com/blueharvest/bank/controller/AccountController.java
+++ src/main/java/com/blueharvest/bank/controller/AccountController.java
@@ -5,6 +5,8 @@ import com.blueharvest.bank.dto.AccountRequestDTO;
 import com.blueharvest.bank.dto.UserInformationResponse;
 import com.blueharvest.bank.model.Account;
 import com.blueharvest.bank.service.AccountService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,19 +15,23 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/account")
 @RequiredArgsConstructor
+@Api(value = "Account Management System", description = "Account management")
 public class AccountController {
     private final AccountService accountService;
 
-    @PostMapping("")
+    @PostMapping
+    @ApiOperation(value = "Open a new account")
     public AccountDTO  openAccount(@RequestBody AccountRequestDTO accountRequestDTO) {
         return accountService.openAccount(accountRequestDTO);
     }
 
     @GetMapping("/{customerId}")
+    @ApiOperation(value = "Get account information by customer ID")
     public UserInformationResponse getAccount(@PathVariable String customerId) {
         return accountService.getAccountByCustomerId(customerId);
     }
-    @GetMapping("")
+    @GetMapping
+    @ApiOperation(value = "Get all accounts")
     public List<AccountDTO> getAllAccount() {
         return accountService.getAccountAllByCustomerId();
     }
